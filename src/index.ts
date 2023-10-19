687f48ff082577f884e427ac1c070253b42e3b18
Luis Abarca <luisabarca@gmail.com>
2023-10-18 23:44:46 -0600

    add Apollo GraphQL server as express middleware

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import path from "path";
+import http from "http";
+import { fileURLToPath } from "url";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { typeDefs, resolvers } from "./graphql";
+import { SERVER_PORT } from "./constants";
+
+(async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  const imagesPath = `${path.dirname(fileURLToPath(import.meta.url))}/images`;
+
+  // Serve local images files
+  app.use(
+    "/images",
+    express.static(imagesPath, {
+      extensions: ["svg", "png", "jpg"],
+    })
+  );
+
+  /**
+   * 
+   * Mount Graphql server
+   * 
+   */
+  app.use(
+    "/graphql",
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:4000",
+        "https://studio.apollographql.com",
+      ],
+    }),
+    bodyParser.json({ limit: "10mb" }),
+    expressMiddleware(server)
+  );
+
+  httpServer.listen(
+    {
+      port: SERVER_PORT,
+    },
+    () => {
+      console.log(`GraphQL Server running at: localhost:${SERVER_PORT}`);
+    }
+  );
+})();
